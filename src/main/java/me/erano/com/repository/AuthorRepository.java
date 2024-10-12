e873ac49c4eefc5808f196c6d7caf57abc876df4
Erano01 <muhammedalisert01@gmail.com>
2024-10-12 03:09:23 +0300

    Author & Book mappings with hibernate and thier migration script init via flyway.

--- src/main/java/me/erano/com/repository/AuthorRepository.java
+++ src/main/java/me/erano/com/repository/AuthorRepository.java
@@ -1,11 +1,9 @@
-package me.erano.com.repositories;
+package me.erano.com.repository;
 
 import me.erano.com.domain.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface BookRepository extends JpaRepository<Author,Long> {
-
-
+public interface AuthorRepository extends JpaRepository<Author,Long> {
 }
--- src/main/java/me/erano/com/repository/BookRepository.java
+++ src/main/java/me/erano/com/repository/BookRepository.java
@@ -1,9 +1,11 @@
-package me.erano.com.repositories;
+package me.erano.com.repository;
 
 import me.erano.com.domain.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface AuthorRepository extends JpaRepository<Author,Long> {
+public interface BookRepository extends JpaRepository<Author,Long> {
+
+
 }
